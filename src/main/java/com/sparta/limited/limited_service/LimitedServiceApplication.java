bdf54b21a58934a8ef0c013385b156c821808871
sohyun119 <sh-119@naver.com>
2025-04-11 00:20:05 +0900

    init commit

---
+++ src/main/java/com/sparta/limited/limited_service/LimitedServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.limited.limited_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitedServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitedServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/limited/limited_service/LimitedServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.limited.limited_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitedServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
