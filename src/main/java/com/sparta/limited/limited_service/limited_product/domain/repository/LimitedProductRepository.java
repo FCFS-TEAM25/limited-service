278f554a15feda721c13ffd5415a0f4418346502
sohyun119 <sh-119@naver.com>
2025-04-13 23:32:15 +0900

    ✨ feat [#2] : 한정수량 상품 단건 조회 Repository 구현

--- src/main/java/com/sparta/limited/limited_service/limited_product/domain/repository/LimitedProductRepository.java
+++ src/main/java/com/sparta/limited/limited_service/limited_product/domain/repository/LimitedProductRepository.java
@@ -1,8 +1,11 @@
 package com.sparta.limited.limited_service.limited_product.domain.repository;
 
 import com.sparta.limited.limited_service.limited_product.domain.model.LimitedProduct;
+import java.util.UUID;
 
 public interface LimitedProductRepository {
 
     void save(LimitedProduct limitedProduct);
+
+    LimitedProduct findById(UUID limitedProductId);
 }
--- src/main/java/com/sparta/limited/limited_service/limited_product/infrastructure/repository/LimitedProductRepositoryImpl.java
+++ src/main/java/com/sparta/limited/limited_service/limited_product/infrastructure/repository/LimitedProductRepositoryImpl.java
@@ -1,8 +1,10 @@
 package com.sparta.limited.limited_service.limited_product.infrastructure.repository;
 
+import com.sparta.limited.limited_service.limited_product.domain.exception.LimitedProductNotFoundException;
 import com.sparta.limited.limited_service.limited_product.domain.model.LimitedProduct;
 import com.sparta.limited.limited_service.limited_product.domain.repository.LimitedProductRepository;
 import com.sparta.limited.limited_service.limited_product.infrastructure.persistence.JpaLimitedProductRepository;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -15,5 +17,11 @@ public class LimitedProductRepositoryImpl implements LimitedProductRepository {
     @Override
     public void save(LimitedProduct limitedProduct) {
         jpaLimitedProductRepository.save(limitedProduct);
+    }
+
+    @Override
+    public LimitedProduct findById(UUID limitedProductId) {
+        return jpaLimitedProductRepository.findById(limitedProductId)
+            .orElseThrow(() -> new LimitedProductNotFoundException(limitedProductId));
     }
 }
