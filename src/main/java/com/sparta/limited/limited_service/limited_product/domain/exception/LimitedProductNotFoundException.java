b1eeb0f1bfe683c0a64e9e9edf4b955d44e83098
sohyun119 <sh-119@naver.com>
2025-04-13 23:30:05 +0900

    ✨ feat [#2] : 한정수량 상품이 없는 경우의 예외처리 커스텀 클래스 생성

---
+++ src/main/java/com/sparta/limited/limited_service/limited_product/domain/exception/LimitedProductNotFoundException.java
@@ -0,0 +1,13 @@
+package com.sparta.limited.limited_service.limited_product.domain.exception;
+
+import com.sparta.limited.common_module.exception.BusinessException;
+import com.sparta.limited.common_module.exception.ErrorCode;
+import java.util.UUID;
+
+public class LimitedProductNotFoundException extends BusinessException {
+
+    public LimitedProductNotFoundException(UUID limitedProductId) {
+        super(ErrorCode.RESOURCES_NOT_FOUND,
+            "Limited Product Not Found - id : {} " + limitedProductId);
+    }
+}
