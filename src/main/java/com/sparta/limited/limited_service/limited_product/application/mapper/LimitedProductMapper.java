4261afe96eada6406cb406da925ec78e622e2275
sohyun119 <sh-119@naver.com>
2025-04-13 22:15:36 +0900

    ✨ feat [#1] : 한정수량 상품 생성 Mapper 생성 - toCreateEntity - toCreateResponse

---
+++ src/main/java/com/sparta/limited/limited_service/limited_product/application/mapper/LimitedProductMapper.java
@@ -0,0 +1,26 @@
+package com.sparta.limited.limited_service.limited_product.application.mapper;
+
+import com.sparta.limited.limited_service.limited_product.application.dto.request.LimitedProductCreateRequest;
+import com.sparta.limited.limited_service.limited_product.application.dto.response.LimitedProductCreateResponse;
+import com.sparta.limited.limited_service.limited_product.application.service.product.dto.ProductInfo;
+import com.sparta.limited.limited_service.limited_product.domain.model.LimitedProduct;
+
+public class LimitedProductMapper {
+
+    public static LimitedProduct toCreateEntity(
+        ProductInfo productInfo,
+        LimitedProductCreateRequest request) {
+
+        return LimitedProduct.of(productInfo.productId(), productInfo.title(),
+            productInfo.description(), productInfo.price(),
+            request.getQuantity());
+    }
+
+    public static LimitedProductCreateResponse toCreateResponse(LimitedProduct limitedProduct) {
+        return LimitedProductCreateResponse.of(limitedProduct.getId(),
+            limitedProduct.getProductId(), limitedProduct.getTitle(),
+            limitedProduct.getDescription(), limitedProduct.getPrice(),
+            limitedProduct.getQuantity());
+    }
+
+}
