a69e4d3198a7e291f70ad34b5cb3c0d79f9777e8
sohyun119 <sh-119@naver.com>
2025-04-14 13:10:55 +0900

    🩹 fix [#12] : feign dto -> 내부 dto 변경시 매개변수 변경 - 인프라 레이어 타입을 직접 가져오지 못하도록 내부 dto 매개변수 변경 - ㄴ 인프라<->어플리케이션 레이어 참조되는 것은 client service 에서만 되도록 - product feign client를 호출하는 담당 service의 네이밍 변경

--- src/main/java/com/sparta/limited/limited_service/limited_product/application/service/product/ProductClientService.java
+++ src/main/java/com/sparta/limited/limited_service/limited_product/application/service/product/ProductClientService.java
@@ -2,6 +2,7 @@ package com.sparta.limited.limited_service.limited_product.application.service.p
 
 import com.sparta.limited.limited_service.limited_product.application.service.product.dto.ProductInfo;
 import com.sparta.limited.limited_service.limited_product.infrastructure.feign.ProductFeignClient;
+import com.sparta.limited.limited_service.limited_product.infrastructure.feign.dto.ProductReadResponse;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,12 +11,14 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j(topic = "feign client : call product-service")
 @RequiredArgsConstructor
-public class ProductService {
+public class ProductClientService {
 
     private final ProductFeignClient productFeignClient;
 
     public ProductInfo getProduct(UUID productId) {
-        return ProductInfo.from(productFeignClient.getProduct(productId));
+        ProductReadResponse response = productFeignClient.getProduct(productId);
+        return ProductInfo.from(response.getProductId(), response.getTitle(),
+            response.getDescription(), response.getPrice());
     }
 
 }
--- src/main/java/com/sparta/limited/limited_service/limited_product/application/service/product/dto/ProductInfo.java
+++ src/main/java/com/sparta/limited/limited_service/limited_product/application/service/product/dto/ProductInfo.java
@@ -1,6 +1,5 @@
 package com.sparta.limited.limited_service.limited_product.application.service.product.dto;
 
-import com.sparta.limited.limited_service.limited_product.infrastructure.feign.dto.ProductReadResponse;
 import java.math.BigDecimal;
 import java.util.UUID;
 
@@ -13,8 +12,8 @@ public record ProductInfo(
 
 ) {
 
-    public static ProductInfo from(ProductReadResponse response) {
-        return new ProductInfo(response.getProductId(), response.getTitle(),
-            response.getDescription(), response.getPrice());
+    public static ProductInfo from(UUID productId, String title, String description,
+        BigDecimal price) {
+        return new ProductInfo(productId, title, description, price);
     }
 }
