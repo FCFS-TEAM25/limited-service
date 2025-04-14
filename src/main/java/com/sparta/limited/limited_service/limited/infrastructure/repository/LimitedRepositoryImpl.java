e8bcb5c9b3ac1fc014d48c789c3be52c0eda85ae
sohyun119 <sh-119@naver.com>
2025-04-14 17:42:57 +0900

    ✨ feat [#5] : 한정판매 이벤트 조회시 필요한 Repository 구현

--- src/main/java/com/sparta/limited/limited_service/limited/infrastructure/repository/LimitedRepositoryImpl.java
+++ src/main/java/com/sparta/limited/limited_service/limited/infrastructure/repository/LimitedRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.sparta.limited.limited_service.limited.infrastructure.repository;
 import com.sparta.limited.limited_service.limited.domain.model.Limited;
 import com.sparta.limited.limited_service.limited.domain.repository.LimitedRepository;
 import com.sparta.limited.limited_service.limited.infrastructure.persistence.JpaLimitedRepository;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -15,5 +16,11 @@ public class LimitedRepositoryImpl implements LimitedRepository {
     @Override
     public void save(Limited limited) {
         jpaLimitedRepository.save(limited);
+    }
+
+    @Override
+    public Limited findById(UUID limitedEventId) {
+        return jpaLimitedRepository.findById(limitedEventId)
+            .orElse(null);
     }
 }
