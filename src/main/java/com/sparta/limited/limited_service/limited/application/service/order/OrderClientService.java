c79be4a74cca9603bf3bb4f2e346a74b16a61918
sohyun119 <sh-119@naver.com>
2025-04-15 16:56:02 +0900

    ✨ feat [#8] : 한정판매 구매 : OrderClientService 생성 및 구현 - feign client 호출하여 필요한 값을 추출

---
+++ src/main/java/com/sparta/limited/limited_service/limited/application/service/order/OrderClientService.java
@@ -0,0 +1,27 @@
+package com.sparta.limited.limited_service.limited.application.service.order;
+
+import com.sparta.limited.limited_service.limited.application.dto.request.LimitedOrderRequest;
+import com.sparta.limited.limited_service.limited.application.service.limited_product.dto.LimitedProductInfo;
+import com.sparta.limited.limited_service.limited.infrastructure.feign.OrderFeignClient;
+import com.sparta.limited.limited_service.limited.infrastructure.feign.dto.OrderCreateRequest;
+import com.sparta.limited.limited_service.limited.infrastructure.feign.dto.OrderCreateResponse;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderClientService {
+
+    private final OrderFeignClient orderFeignClient;
+
+    public UUID createOrder(Long userId, LimitedProductInfo productInfo,
+        LimitedOrderRequest orderRequest) {
+        OrderCreateRequest orderCreateRequest = new OrderCreateRequest(productInfo.productId(),
+            productInfo.price(), orderRequest.getQuantity());
+
+        OrderCreateResponse response = orderFeignClient.createOrder(userId, orderCreateRequest);
+        return response.orderId();
+    }
+
+}
