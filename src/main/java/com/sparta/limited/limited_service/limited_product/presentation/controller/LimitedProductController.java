0435505b7d4f287f9db8bad22b205e808c4a29e3
sohyun119 <sh-119@naver.com>
2025-04-17 10:47:32 +0900

    🩹 fix [#24] : CREATE시 헤더에 생성된 값을 볼 수 있는 엔드포인트 함께 응답

--- src/main/java/com/sparta/limited/limited_service/limited_product/presentation/controller/LimitedProductController.java
+++ src/main/java/com/sparta/limited/limited_service/limited_product/presentation/controller/LimitedProductController.java
@@ -5,9 +5,9 @@ import com.sparta.limited.limited_service.limited_product.application.dto.reques
 import com.sparta.limited.limited_service.limited_product.application.dto.response.LimitedProductCreateResponse;
 import com.sparta.limited.limited_service.limited_product.application.dto.response.LimitedProductReadResponse;
 import com.sparta.limited.limited_service.limited_product.application.service.LimitedProductService;
+import java.net.URI;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequiredArgsConstructor
@@ -33,7 +34,13 @@ public class LimitedProductController {
 
         LimitedProductCreateResponse response = limitedProductService.createLimitedProduct(
             productId, request);
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentContextPath()
+            .path("/api/v1/limited-products/{id}")
+            .buildAndExpand(response.getId())
+            .toUri();
+
+        return ResponseEntity.created(uri).body(response);
     }
 
     @GetMapping("/{limitedProductId}")
