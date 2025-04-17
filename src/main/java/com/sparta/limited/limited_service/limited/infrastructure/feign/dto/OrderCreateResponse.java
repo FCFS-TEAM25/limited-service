9cd86824b1ba0389bca9893da2a8bb1835ff8549
sohyun119 <sh-119@naver.com>
2025-04-17 20:07:53 +0900

    🩹 fix [#23] : Order feign client 관련 수정

--- src/main/java/com/sparta/limited/limited_service/limited/infrastructure/feign/dto/OrderCreateResponse.java
+++ src/main/java/com/sparta/limited/limited_service/limited/infrastructure/feign/dto/OrderCreateResponse.java
@@ -1,6 +1,7 @@
 package com.sparta.limited.limited_service.limited.infrastructure.feign.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sparta.limited.limited_service.limited.application.service.order.model.OrderType;
 import java.math.BigDecimal;
 import java.util.UUID;
 
@@ -12,7 +13,7 @@ public record OrderCreateResponse(
     Long userId,
     String username,
     String address,
-    String orderType,
+    OrderType orderType,
     String status,
     BigDecimal price,
     Integer quantity) {
