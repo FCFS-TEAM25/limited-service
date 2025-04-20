fe88dcc6ea7ebff073c16fc631b3d12b6d010953
sohyun119 <sh-119@naver.com>
2025-04-20 20:36:37 +0900

    ♻️ refactor [#28] : Feign Client url 변경

--- src/main/java/com/sparta/limited/limited_service/limited/infrastructure/feign/OrderFeignClient.java
+++ src/main/java/com/sparta/limited/limited_service/limited/infrastructure/feign/OrderFeignClient.java
@@ -1,18 +1,18 @@
 package com.sparta.limited.limited_service.limited.infrastructure.feign;
 
+import com.sparta.limited.common_module.common.annotation.CurrentUserId;
 import com.sparta.limited.limited_service.limited.infrastructure.feign.dto.OrderCreateRequest;
 import com.sparta.limited.limited_service.limited.infrastructure.feign.dto.OrderCreateResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "order-service", url = "localhost:19094/api/v1/internal/orders")
+@FeignClient(name = "order-service")
 public interface OrderFeignClient {
 
-    @PostMapping
+    @PostMapping("/api/v1/internal/orders")
     OrderCreateResponse createOrder(
-        @RequestHeader("X-User-Id") Long userId,
+        @CurrentUserId Long userId,
         @RequestBody OrderCreateRequest request);
 
 }
