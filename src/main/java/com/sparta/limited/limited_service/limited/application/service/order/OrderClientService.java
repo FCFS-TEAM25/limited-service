2d720b40567dc8e1718e4afcf35d538ad191f2f1
sohyun119 <sh-119@naver.com>
2025-04-23 00:40:54 +0900

    ✨ feat [#37] : Order-service feign client try-catch 처리 - feign client 호출 service 에서 try-catch 처리 - Feign client Exception 생성

--- src/main/java/com/sparta/limited/limited_service/limited/application/service/order/OrderClientService.java
+++ src/main/java/com/sparta/limited/limited_service/limited/application/service/order/OrderClientService.java
@@ -2,9 +2,11 @@ package com.sparta.limited.limited_service.limited.application.service.order;
 
 import com.sparta.limited.limited_service.limited.application.dto.request.LimitedOrderRequest;
 import com.sparta.limited.limited_service.limited.application.service.limited_product.dto.LimitedProductInfo;
+import com.sparta.limited.limited_service.limited.domain.exception.OrderServiceFeignClientException;
 import com.sparta.limited.limited_service.limited.infrastructure.feign.OrderFeignClient;
 import com.sparta.limited.limited_service.limited.infrastructure.feign.dto.OrderCreateRequest;
 import com.sparta.limited.limited_service.limited.infrastructure.feign.dto.OrderCreateResponse;
+import feign.FeignException.FeignClientException;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,8 +22,12 @@ public class OrderClientService {
         OrderCreateRequest orderCreateRequest = new OrderCreateRequest(productInfo.productId(),
             productInfo.price(), orderRequest.getQuantity());
 
-        OrderCreateResponse response = orderFeignClient.createOrder(userId, orderCreateRequest);
-        return response.orderId();
+        try {
+            OrderCreateResponse response = orderFeignClient.createOrder(userId, orderCreateRequest);
+            return response.orderId();
+        } catch (FeignClientException e) {
+            throw new OrderServiceFeignClientException();
+        }
     }
 
 }
